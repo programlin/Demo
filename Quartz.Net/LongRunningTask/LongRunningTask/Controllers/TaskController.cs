f077c75c28578dfda0f3c12f71db8c884a8f43d0
programlin <programlin@gmail.com>
2021-01-11 22:10:38 +0800

    temp

---
+++ Quartz.Net/LongRunningTask/LongRunningTask/Controllers/TaskController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using LongRunningTask.Jobs;
+using Microsoft.AspNetCore.Mvc;
+using Quartz;
+
+namespace LongRunningTask.Controllers
+{
+    public class TaskController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ScheduleJob()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> NewTask([FromServices] ISchedulerFactory schedulerFactory, CancellationToken cancellationToken)
+        {
+            var scheduler = await schedulerFactory.GetScheduler(cancellationToken);
+
+
+            //sched.scheduleJob(job, trigger);
+
+            //await scheduler.ScheduleJob(trigger => trigger
+            //    .StartAt(DateBuilder.EvenSecondDate(DateTimeOffset.UtcNow.AddSeconds(7)))
+            //);
+                
+
+            return View();
+        }
+    }
+}
---
+++ Quartz.Net/LongRunningTask/LongRunningTask/Jobs/DemoJob.cs
@@ -0,0 +1,21 @@
+﻿using Quartz;
+using System;
+using System.Threading.Tasks;
+
+namespace LongRunningTask.Jobs
+{
+    public class DemoJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await Task.Run(() =>
+            {
+                for (var i = 0; i < 5; i++)
+                {
+                    Console.WriteLine($"第{i}次執行");
+                    context.CancellationToken.WaitHandle.WaitOne(TimeSpan.FromSeconds(2));
+                }
+            });
+        }
+    }
+}
